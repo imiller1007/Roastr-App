b79f53fba42485b8f7dc459195abde7bee476c10
astie086 <astie086@gmail.com>
2018-03-31 13:31:12 -0700

    schema update

--- schema1.sql
+++ schema1.sql
@@ -1,24 +1,23 @@
--- Created the DB "post table" 
-CREATE DATABASE post_table;
-USE post_table;
+-- Created the DB "roaster" 
+
+CREATE DATABASE roaster;
+USE roaster;
 
 -- Created the table "post table" 
-CREATE TABLE burgers (
-  postID Int( 10 ) AUTO_INCREMENT NOT NULL,
-  userID VARCHAR( 10 ) NOT NULL,
-  post VARCHAR NOT NULL,
+
+CREATE TABLE products (
+  postid INT NOT NULL,
+  userid VARCHAR(45) NULL,
+  post VARCHAR( 100 ) NOT NULL,
   upVote Int( 10 ),
   downVote Int(10)
 );
 
--- Created the DB "user table" 
-CREATE DATABASE user_table;
-USE user_table;
-
 -- Created the table "user" 
+
 CREATE TABLE users (
-  userID Int( 10 ) AUTO_INCREMENT NOT NULL,
-  userName VARCHAR( 100 ) NOT NULL,
-  numPosts VARCHAR NOT NULL,
-  postsCount Int( 10 ),
+  userID INT NOT NULL,
+  userName VARCHAR(45) NOT NULL,
+  numPosts int NOT NULL,
+  postsCount Int( 10 )
 );
